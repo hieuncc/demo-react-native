280706506ace2aba31d5825c253f47b399de1d41
spritelytrung <trung@spritely.co>
2021-12-15 23:09:58 +0700

    break component card item

---
+++ src/pages/home/template/header_item/style.js
@@ -0,0 +1,7 @@
+export const style = {
+  color: "#181725",
+  fontSize: 16,
+  lineHeight: 18,
+  letterSpacing: 0.1,
+  fontWeight: "bold",
+}
---
+++ src/pages/home/template/subtitle_item/style.js
@@ -0,0 +1,7 @@
+export const style = {
+  width: "100%",
+  color: "#7C7C7C",
+  fontSize: 14,
+  lineHeight: 18,
+  letterSpacing: 0.1,
+}
